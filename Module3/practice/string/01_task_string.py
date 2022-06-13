3ea7016e0ca017925c3cf39c42f98762aa9092d9
egorushka123 <106911096+egorushka123@users.noreply.github.com>
2022-06-13 16:14:51 +0300

    Update 01_task_string.py

--- Module3/practice/string/01_task_string.py
+++ Module3/practice/string/01_task_string.py
@@ -8,3 +8,11 @@ surname = input("Фамилия: ")
 
 
 print(name, surname)
+
+
+name = input("Имя: ")
+surname = input("Фамилия: ")
+new_name = name.title()
+new_surname = surname. title() 
+
+print(new_name,new_surname)
